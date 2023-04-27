c0d4b264093729efc7e59e8204853e1f9bc898c3
Amalia Gimranova <amaliagimranova@MacBook-Pro-Amalia.local>
2023-04-27 14:33:54 +0300

    first commit

---
+++ TipsCalculate/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by Amalia Gimranova on 19.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
